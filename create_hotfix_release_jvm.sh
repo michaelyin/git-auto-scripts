8dc15ac3bba129132fa97ea1b9e597a5bcf16845
xuecheng.yin <xuecheng.yin@elanking.com>
2018-03-14 08:30:42 +0800

    add scripts to automate git operations

---
+++ create_hotfix_release_jvm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#create hotfix or release branch for jvm related projects
+create_a_branch()
+{
+  git_link=$link_prefix$1$link_affix
+  echo "gitlab link: $git_link"
+  project_name=$1
+  branch=$2
+  
+  echo "create $branch for project $project_name ..."
+  git clone $git_link
+  cd $project_name
+  git checkout -b $branch
+  git push origin $branch
+
+  echo "done creating $branch with $project_name"
+  cd ..
+  rm -rf $project_name
+}
+
+if [ $# == 0 ]; then
+    echo -e "usage: $0 branch_name (hotfix or release) "
+    exit 1
+fi
+
+branch_to_be_created=$1
+echo "branch to be created: $branch_to_be_created"
+
+link_prefix="ssh://git@git.elanking.com:922/platform/"
+link_affix=".git"
+
+create_a_branch yoo $branch_to_be_created
+create_a_branch yoo-job $branch_to_be_created
---
+++ create_hotfix_release_web.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#create hotfix or release branch for web related projects
+create_a_branch()
+{
+  git_link=$link_prefix$1$link_affix
+  echo "gitlab link: $git_link"
+  project_name=$1
+  branch=$2
+  
+  COMMENTS=$@
+  echo "create $branch for project $project_name ..."
+  git clone $git_link
+  cd $project_name
+  git checkout -b $branch
+  git push origin $branch
+
+  echo "done creating $branch with $project_name"
+  cd ..
+  rm -rf $project_name
+}
+
+if [ $# == 0 ]; then
+    echo -e "usage: $0 branch_name (hotfix or release) "
+    exit 1
+fi
+
+branch_to_be_created=$1
+echo "branch to be created: $branch_to_be_created"
+
+link_prefix="ssh://git@git.elanking.com:922/web/"
+link_affix=".git"
+
+create_a_branch yoomathm $branch_to_be_created
+create_a_branch zycon $branch_to_be_created
+create_a_branch rescon $branch_to_be_created
+create_a_branch operation $branch_to_be_created
+create_a_branch yoomath $branch_to_be_created
+create_a_branch yoomath-wx $branch_to_be_created
+create_a_branch yoomath-m $branch_to_be_created
+create_a_branch channel-sales $branch_to_be_created
+
